7090a614eff9673b52794f2266979355c927ff2d
Vog2k <99256921+Vog2k@users.noreply.github.com>
2022-05-23 15:07:03 +1200

    new

--- Timothys Box Calculator.py
+++ Timothys Box Calculator.py
@@ -1,57 +1,54 @@
-num1 = int(input("Please enter number:"))  # Num1 is my variable, requires users input
-print()  # Spaces in between to make the terminal look less cramped
-num2 = int(input("Please enter second number:"))  # Num2 is my variable, requires users input
-print()  # Spaces in between to make the terminal look less cramped
+# Box Calculator
+# BY Timothy Leatau
 
-result = num1 + num1
+# First variable asking for an input number from the user
+first_number = int(input("Please enter you're first number :"))
 
+print() # Space to make the command look more free
+
+# Second variable asking for an input number from the user
+second_number = int(input("Please enter you're second number :"))
+
+print() # Space to make the command look more free
+
+# This line will show the user the equation then convert to boxes
+print("Your results\n")
+
+# Third variable adding both variables and storing it in a new variable named result
+result = first_number + second_number
+
+print(first_number, "+", second_number, "=", result)
+print("-------------------------------------------")
+print()
+
+# Big box containg the result of the equation entered and a division sign along with the size being 5
 big = int(result / 5)
 
 medium = int(result / 3)
 
 small = int(result / 1)
-
+# Forth variable convering the name to x
 x = result
-
-if x > 0:
-    for i in range(5, x):
+"""Trying to make the Big box fill first before heading to the next avaliable number being 3/medium box ect"""
+if x > 0 :
+    for i in range(1, x):
         if x % 5 == 0:
-            print("Big box |5|:", big)
-
-            print("Small box |1|:", small)
+            print()
+            print(big, "[Big Boxes filled]")
             break
-        elif x % 3 == 0:
-            print("Medium box |3|:", medium)
-            print("Small box |1|:", small)
-
+        elif x % 3 == 0 :
+            print()
+            print(medium, "[Medium Boxes filled]")
             break
         elif x % 1 == 0:
-            print("Small box |1|:", small)
+            print(small, "[Small Boxes filled]")
             break
-else:
-    print('give a positive number')
-
-"""if x > 0:
-    for i in range(3, x):
-        if x % 3 == 0:
-            print("BIG. MED BOX2",big,  medium)
-            break
-        else:
-            print("ALL BOXES2",big , medium , small)
-            break
-else:
-    print('give a positive number')
-
-if x > 0:
-    for i in range(1, x):
-        if x % 1 == 0:
-            print("SMALL3", small)
-            break
-else:
-    print('give a positive number')"""
 
 
-"""if result >= 0:# is odd use 5s and 3s first
-    print("\nBig Box:", big, "\nMedium Box:", medium, "\nSmall Box", small)
-else:
-    print()"""
+"""print()
+print("Med:- ", medium)
+print()
+
+print()
+print("Small:- ", small)
+"""
